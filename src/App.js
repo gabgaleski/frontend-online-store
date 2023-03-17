6f495ebd50901eebc1c78566ab74b134bff531c4
Wilkson Pedro da Silva <wil.thydus@gmail.com>
2023-03-17 16:42:09 -0300

    requisito 2

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import React from 'react';
+import { Route } from 'react-router-dom';
 import './App.css';
+import Home from './components/Home';
 import { getCategories } from './services/api';
 
 class App extends React.Component {
   render() {
     console.log(getCategories);
     return (
-      <div className="App" />
+      <div className="App">
+        <Route exact path="/" component={ Home } />
+      </div>
     );
   }
 }
-// olá
+
 export default App;

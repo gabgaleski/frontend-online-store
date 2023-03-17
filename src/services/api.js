9e2f8cbd48e9e847f8feecb75452b3e6a1c11a97
Wilkson Pedro da Silva <wil.thydus@gmail.com>
2023-03-17 16:01:15 -0300

    requisisto 1

--- src/services/api.js
+++ src/services/api.js
@@ -1,9 +1,13 @@
 export async function getCategories() {
-  // Implemente aqui
+  const data = await fetch('https://api.mercadolibre.com/sites/MLB/categories');
+  const categories = await data.json();
+  return categories;
 }
 
-export async function getProductsFromCategoryAndQuery(/* categoryId, query */) {
-  // Implemente aqui! Quando o fizer, descomente os parâmetros que essa função recebe
+export async function getProductsFromCategoryAndQuery(search) {
+  const data = await fetch(`https://api.mercadolibre.com/sites/MLB/search?q=${search}`);
+  const categoriesQuery = await data.json();
+  return categoriesQuery;
 }
 
 export async function getProductById() {

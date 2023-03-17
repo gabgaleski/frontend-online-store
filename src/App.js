11a580d61b9bbd30a004923d9a8ee9e3ddfe9513
Thálisson Gonçalves <thalissongsilva70@gmail.com>
2023-03-17 19:15:19 -0300

    Feito requisito 06 - Group 16

--- src/App.js
+++ src/App.js
@@ -3,11 +3,9 @@ import { Route } from 'react-router-dom';
 import './App.css';
 import Home from './components/Home';
 import ShoppingCart from './components/ShoppingCart';
-import { getCategories } from './services/api';
 
 class App extends React.Component {
   render() {
-    console.log(getCategories);
     return (
       <div className="App">
         <Route exact path="/" component={ Home } />
---
+++ src/components/QueryProduct.jsx
@@ -0,0 +1,65 @@
+import React from 'react';
+import { getProductsFromCategoryAndQuery } from '../services/api';
+
+class QueryProduct extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      inputValue: '',
+      productsArray: [],
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange({ target }) {
+    const { name, value } = target;
+    this.setState({
+      [name]: value,
+    });
+  }
+
+  getApiProducts = async (search) => {
+    const getProducts = await getProductsFromCategoryAndQuery(search);
+    this.setState({
+      productsArray: getProducts.results,
+    });
+  };
+
+  render() {
+    const { inputValue, productsArray } = this.state;
+    return (
+      <>
+        <label htmlFor="inputValue">
+          <input
+            type="text"
+            data-testid="query-input"
+            name="inputValue"
+            id="inputValue"
+            value={ inputValue }
+            onChange={ this.handleChange }
+          />
+        </label>
+        <button
+          data-testid="query-button"
+          onClick={ () => this.getApiProducts(inputValue) }
+        >
+          Pesquisar
+        </button>
+        {
+          productsArray.length > 0
+            ? productsArray.map((product) => (
+              <div data-testid="product" key={ product.id }>
+                <h1>{ product.title }</h1>
+                <img src={ product.thumbnail } alt={ product.title } />
+                <p>{ product.price }</p>
+              </div>
+            )) : <p>Nenhum produto foi encontrado</p>
+        }
+      </>
+    );
+  }
+}
+
+export default QueryProduct;

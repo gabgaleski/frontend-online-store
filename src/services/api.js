e9f8f55bafdfbdbc4d9218b42b102442f4157939
Wilkson Pedro da Silva <wil.thydus@gmail.com>
2023-03-17 16:16:46 -0300

    requisito 2

--- src/services/api.js
+++ src/services/api.js
@@ -13,4 +13,5 @@ export async function getProductsFromCategoryAndQuery(search) {
 export async function getProductById() {
   // Esta implementação específica não é avaliada, mas pode ajudar você 🙂
   // Atenção: essa função não deverá ser chamada na tela do carrinho de compras.
+  // oi.
 }

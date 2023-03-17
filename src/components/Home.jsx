58f10c898b4054aa7b1f68066e18a20766286fa2
Thálisson Gonçalves <thalissongsilva70@gmail.com>
2023-03-17 18:15:26 -0300

    Feito requisito 04

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,21 +1,54 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import { getCategories } from '../services/api';
 
 export default class Home extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      categories: [],
+    };
+  }
+
+  async componentDidMount() {
+    const result = await this.getApiCategory();
+    this.setState({
+      categories: result,
+    });
+  }
+
+  getApiCategory = async () => {
+    const categories = await getCategories();
+    return categories;
+  };
+
   render() {
+    const { categories } = this.state;
     return (
       <>
-        <Link
-          to="/shoppingcart"
-          data-testid="shopping-cart-button"
-        >
-          Carrinho de compras
-        </Link>
-        <h1
-          data-testid="home-initial-message"
-        >
-          Digite algum termo de pesquisa ou escolha uma categoria.
-        </h1>
+        <div>
+          <Link
+            to="/shoppingcart"
+            data-testid="shopping-cart-button"
+          >
+            Carrinho de compras
+          </Link>
+          <h1
+            data-testid="home-initial-message"
+          >
+            Digite algum termo de pesquisa ou escolha uma categoria.
+          </h1>
+        </div>
+        {
+          categories.map((category) => (
+            <label data-testid="category" htmlFor={ category.id } key={ category.id }>
+              { category.name }
+              <input type="radio" id={ category.id } />
+            </label>
+          ))
+        }
+        <div />
       </>
     );
   }

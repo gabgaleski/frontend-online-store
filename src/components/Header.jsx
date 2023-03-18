2be2514a3a44ce35360df5aad9727b4838ad72e6
lucopdev <lucopdev@gmail.com>
2023-03-18 14:28:13 -0300

    estilização inicial

---
+++ src/components/Header.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import '../styles/header.css';
+
+class Header extends Component {
+  render() {
+    return (
+      <div className="header">
+        <p>logo</p>
+        <p>links</p>
+        <p>login</p>
+      </div>
+    );
+  }
+}
+
+export default Header;
